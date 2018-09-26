d271cd5893a519a496a68cc4dfeb5764dbef42a5
RenaldoV <u12204359@gmail.com>
2018-09-26 14:33:30 +0200

    Testing product mix report.

--- library/library.php
+++ library/library.php
@@ -265,6 +265,7 @@ function GetProductMix($sumid, $orderby) {
 $SQL = "select lcase(ibrcategoryname) as 'ibrcategoryname', ibritenplu ,ibritemname, ibritemcost , sum(ibrnumsold) as 'ibrnumsold',ibrpricesold as 'ibrpricesold',sum(ibramount) as 'ibramount',sum(ibrsalespercent) as 'ibrsalespercent' from itemsbreakdown where sumid in (".$sumid.") and ibrtimesold >= '".$_SESSION["timefrom"]."' and ibrtimesold < '".$_SESSION["timeto"]."' group by lcase(ibrcategoryname),ibritemname order by ibrcategoryname,".$orderby;
 //echo $SQL;
 $result = db_execsql($SQL);
+echo "<script>console.log('product mix report'+".json_encode($sumid).")</script>";
 return $result;
 }
 
